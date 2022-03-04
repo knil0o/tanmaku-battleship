d2b3488aa57b46fec82cfd3a4578b5e1e0e97638
zhenz <578079901@qq.com>
2022-03-04 19:27:03 +0800

    fix:  鼠标点击位置

--- MainScene.gd
+++ MainScene.gd
@@ -8,20 +8,15 @@ export(PackedScene) var chess_board_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("!!!!!!!!!runing main scene!!!!!!!")
-	var chess_board = chess_board_scene.instance()
-	add_child(chess_board)
-	chess_board.position = Vector2(16,32)
-	#signal连接被调用的方法
-	connect("click_pos", chess_board, "_on_click")
-	pass # Replace with function body.
+	var chess_board1 = chess_board_scene.instance()
+	var chess_board2 = chess_board_scene.instance()
+	add_child(chess_board1)
+	add_child(chess_board2)
+	chess_board1.position = Vector2(64,32)
+	chess_board2.position = Vector2(256,32)
+	pass 
 
 func _process(delta):
 	pass
-func _input(event):
-	if event is InputEventMouseButton:
-		#发送signal
-		emit_signal("click_pos", event)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
--- chessboard/ChessBoard.gd
+++ chessboard/ChessBoard.gd
@@ -7,33 +7,43 @@ var chess_board_scale = Vector2(10, 10)
 
 var normal_ground = 1
 var attacked_ground = 2
+var tile_size = 16
+var item_offset = tile_size / 2
+
 func _ready():
-	for cell in get_used_cells():
-		print("x: "+ str(cell.x) + "y: " + str(cell.y))
+#	for cell in get_used_cells():
+#		print("x: "+ str(cell.x) + "y: " + str(cell.y))
 	
 	var ship = ship_scene.instance()
 	place_tile(chess_board_scale)
-	
-	#add_child(ship)
-	#place_ship(ship, 1, 1, Vector2(0,1))
+	place_ship(ship, 1, 1, Vector2(5,2))
 
 
-#点击事件(MainScene发送的)
-func _on_click(event):
-	var mouse_pos = get_viewport().get_mouse_position()
-	var tile_pos = map_to_world(world_to_map(mouse_pos))
-	print("click world_to_map" + str(world_to_map(mouse_pos)))
-	print("click" + str(tile_pos.x) + ", " + str(tile_pos.y))
-	var ats = attacked_sea_scene.instance()
+
+#收到点击事件
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		var mouse_pos = event.position - position
+		var tile_pos = world_to_map(mouse_pos)
+		print("click world_to_map" + str(world_to_map(mouse_pos)))
+		print("click" + str(tile_pos.x) + ", " + str(tile_pos.y))
+		var ats = attacked_sea_scene.instance()
+		set_cellv(world_to_map(mouse_pos), 2)
 	
+func _process(delta):
+	set_cell(2,2,2)
+
 
 func place_ship(ship, scale_x, scale_y, map_pos: Vector2):
-	ship.position = map_to_world(map_pos)
-	
+	add_child(ship)
+	ship.position = pos_into_cell(map_to_world(map_pos))
 func place_tile(chess_board_scale):
 	for x in chess_board_scale.x:
 		for y in chess_board_scale.y:
-			var sea_tile = sea_scene.instance()
-			add_child(sea_tile)
-			sea_tile.position = Vector2(x, y)
+			set_cell(x, y, 1)
+	
+func pos_into_cell(pos: Vector2):
+	pos.snapped(Vector2.ONE * tile_size)
+	pos += Vector2.ONE * tile_size/2
+	return pos
 	
